26dafd9dd322556832615ef8406eab9610c57735
Yevhen Vahin <ie.s.vagin@gmail.com>
2021-06-02 21:27:09 +0300

    Fixed bug with spark supported extensions check
    Signed-off-by: Yevhen Vahin <ie.s.vagin@gmail.com>

--- utils/spark.py
+++ utils/spark.py
@@ -14,8 +14,8 @@ class SparkDefaults:
 def open_file_as_df(spark:SparkSession, fpath:pathlib.Path) -> DataFrame:
     # NOTE(i.vagin): # Format is based on file extension:
     src_data_format = fpath.suffix.lstrip('.') 
-    if src_data_format in SparkDefaults.SUPPORTED_SRC_FORMATS:
-            raise TypeError("Source file need to have one "
-                           f"of the supported extensions: {SparkDefaults.SUPPORTED_SRC_FORMATS}.")
+    if src_data_format not in SparkDefaults.SUPPORTED_SRC_FORMATS:
+        raise TypeError("Source file need to have one "
+                        f"of the supported extensions: {SparkDefaults.SUPPORTED_SRC_FORMATS}.")
     df = spark.read.format(src_data_format).load(fpath.as_posix())
     return df
